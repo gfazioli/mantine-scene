2d4762419e176fe335e1d492c29f1866e2736e52
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-05-17 12:53:30 +0200

    Updates

--- _next/static/R0jw_K-RMRXv0s_AE1TK_/_buildManifest.js
+++ _next/static/R0jw_K-RMRXv0s_AE1TK_/_buildManifest.js
@@ -1,12 +1,12 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/09som-5t9vfgs.js"
+    "static/chunks/0zcyf1e8bxv9p.js"
   ],
   "/_error": [
     "static/chunks/0t-i9y7uasms7.js"
   ],
   "/fullscreen": [
-    "static/chunks/0m5e5~n4vlv-e.js"
+    "static/chunks/0s32~1d30-a.3.js"
   ],
   "__rewrites": {
     "afterFiles": [],
--- _next/static/chunks/0s32~1d30-a.3.js
+++ _next/static/chunks/0s32~1d30-a.3.js
@@ -1,9 +1,9 @@
 __turbopack_load_page_chunks__("/fullscreen", [
   "static/chunks/0sps5lyfp.prq.js",
   "static/chunks/0naer56rp~298.js",
-  "static/chunks/13ad~mxawifrd.js",
+  "static/chunks/0zz0l1cjndv49.js",
   "static/chunks/0lj73yu6clnd2.js",
-  "static/chunks/0rcrnsj_app51.js",
-  "static/chunks/0~54ix8~4ybk_.js",
-  "static/chunks/turbopack-02k~7bcdzy-v4.js"
+  "static/chunks/026~hxopgv968.js",
+  "static/chunks/0mpcb~hquqc_l.js",
+  "static/chunks/turbopack-04_s527auh2je.js"
 ])
